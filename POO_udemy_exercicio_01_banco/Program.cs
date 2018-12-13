353acd0e982af955c323947f8120ef3f4dcd57b7
Leonardo Rodrigues <leonardorodrigues_f@hotmail.com>
2018-12-13 14:42:32 -0200

    alteracao nas variaveis do objeto contaBancaria

--- POO_udemy_exercicio_01_banco/Program.cs
+++ POO_udemy_exercicio_01_banco/Program.cs
@@ -6,7 +6,7 @@ namespace POO_udemy_exercicio_01_banco
     {
         static void Main(string[] args)
         {
-            ContaBancaria conta;
+            ContaBancaria contaShow;
 
             Console.Write("Digite o número da conta: ");
             int numero = int.Parse(Console.ReadLine());
@@ -21,24 +21,24 @@ namespace POO_udemy_exercicio_01_banco
                 Console.Write("Digite o valor do depósito: R$");
                 double depositoInicial = double.Parse(Console.ReadLine());
 
-                conta = new ContaBancaria(nome, numero, depositoInicial);
+                contaShow = new ContaBancaria(nome, numero, depositoInicial);
             }
             else
             {
-                conta = new ContaBancaria(nome, numero);
+                contaShow = new ContaBancaria(nome, numero);
             }
             Console.WriteLine("Informações do cliente:");
-            Console.WriteLine(conta);
+            Console.WriteLine(contaShow);
             Console.WriteLine();
             Console.Write("Digite um valor para o deposito: R$");
             double deposito = double.Parse(Console.ReadLine());
-            conta.Depositar(deposito);
-            Console.WriteLine(conta);
+            contaShow.Depositar(deposito);
+            Console.WriteLine(contaShow);
             Console.WriteLine();
             Console.Write("Digite um valor para saque: R$");
             double saque = double.Parse(Console.ReadLine());
-            conta.Sacar(saque);
-            Console.WriteLine("Datos sobre a conta: " + conta);
+            contaShow.Sacar(saque);
+            Console.WriteLine("Datos sobre a conta: " + contaShow);
             Console.ReadLine();
         }
     }
