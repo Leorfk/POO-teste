83f215997ceeeb8f5f4c0a95c0d31afe2b821a16
Leonardo Rodrigues <leonardorodrigues_f@hotmail.com>
2018-12-13 11:31:10 -0200

    Projeto de teste do git

---
+++ POO_udemy_exercicio_01_banco/ContaBancaria.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_udemy_exercicio_01_banco
+{
+    class ContaBancaria
+    {
+        public string Nome { get; set; }
+        public int NumeroDaConta { get; private set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(string nome, int numeroDaConta)
+        {
+            Nome = nome;
+            NumeroDaConta = numeroDaConta;
+        }
+        public ContaBancaria(string nome, int numeroDaConta, double saldo) : this(nome, numeroDaConta)
+        {//esse 'this' serve para tirarmos a redundância de escrever as atribuições mais vezes
+            //Saldo = saldo;
+            Depositar(saldo);//é melhor usar o método para facilitar a manutenção
+        }
+
+        public void Depositar(double deposito)
+        {
+            Saldo += deposito;
+        }
+
+        public void Sacar(double saque)
+        {
+            Saldo = Saldo - (saque + 5.00);
+        }
+
+        public override string ToString()
+        {
+            return "Cliente: " + Nome + "\nNúmero da conta: " + NumeroDaConta + "\nSaldo: R$" + Saldo;
+        }
+    }
+}
---
+++ POO_udemy_exercicio_01_banco/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace POO_udemy_exercicio_01_banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+
+            Console.Write("Digite o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Será feito um depósito inicial [S/N] ?");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 'S' || resp == 's')
+            {
+                Console.Write("Digite o valor do depósito: R$");
+                double depositoInicial = double.Parse(Console.ReadLine());
+
+                conta = new ContaBancaria(nome, numero, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(nome, numero);
+            }
+            Console.WriteLine("Informações do cliente:");
+            Console.WriteLine(conta);
+            Console.WriteLine();
+            Console.Write("Digite um valor para o deposito: R$");
+            double deposito = double.Parse(Console.ReadLine());
+            conta.Depositar(deposito);
+            Console.WriteLine(conta);
+            Console.WriteLine();
+            Console.Write("Digite um valor para saque: R$");
+            double saque = double.Parse(Console.ReadLine());
+            conta.Sacar(saque);
+            Console.WriteLine(conta);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
